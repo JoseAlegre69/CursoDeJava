8343223aed7b97b45363c79e1ffced9e37a65cad
JoseAlegre69 <joseignacioalegre4@gmail.com>
2022-08-23 19:52:07 -0300

    ej4

--- TPs/src/TP4/Main.java
+++ TPs/src/TP4/Main.java
@@ -1,15 +1,17 @@
+package TP4;
+
 import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
-        
+
+        System.out.println("Hello world!");
         String nombre = "Jose";
         int edad = 22;
         double salario = 10000000;
         boolean carnet = true;
         JOptionPane.showMessageDialog(null,
-                "Mi nombre es: "+nombre+", tengo " + edad +" años" +", me gustaria ganar "+salario+" centavos."+" Carnet? "+carnet);
-
+                "Mi nombre es: "+nombre+", tengo " + edad +" " + "años" +", me gustaria ganar " +salario+" centavos."+" Carnet? "+carnet);
 
     }
 }
